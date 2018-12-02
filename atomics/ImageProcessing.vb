46c3cf0c5e430331b8fdb210847002da9caf7e25
albusmw <albusmw@gmx.de>
2018-12-02 23:45:17 +0100

    my first real commit - only added comments ...

--- atomics/ImageProcessing.vb
+++ atomics/ImageProcessing.vb
@@ -3,7 +3,13 @@ Option Strict On
 
 Public Class ImageProcessing
 
-    '''<summary>Calculate basic bayer statistics on the passed INT32 data.</summary>
+    '''<summary>Calculate basic bayer statistics on the passed data matrix.</summary>
+    '''<param name="Data">Matrix of data - 2D matrix what contains the raw sensor data.</param>
+    '''<param name="OffsetX">0-based X offset where to start from.</param>
+    '''<param name="OffsetY">0-based Y offset where to start from.</param>
+    '''<param name="SteppingX">Step size in X direction - typically 2 for a normal RGGB bayer matrix.</param>
+    '''<param name="SteppingY">Step size in X direction - typically 2 for a normal RGGB bayer matrix.</param>
+    '''<returns>A sorted dictionary which contains all found values of type T in the Data matrix and its count.</returns>
     Public Shared Function BayerStatistics(Of T)(ByRef Data(,) As T, ByVal OffsetX As Integer, ByVal SteppingX As Integer, ByVal OffsetY As Integer, ByVal SteppingY As Integer) As Dictionary(Of T, UInt32)
 
         Dim OneMore As UInt32 = CType(1, UInt32)
