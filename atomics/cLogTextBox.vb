4785cf0d3e86be5319f906b16c2a0a359776f700
Martin Weiss <albusmw@gmx.de>
2020-12-26 01:40:07 +0100

    cLogTextBox added (used in AstroBin search ...)

---
+++ atomics/cLogTextBox.vb
@@ -0,0 +1,22 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class cLogTextBox : Inherits TextBox
+
+    Private TextBuilder As New System.Text.StringBuilder
+
+    Public Sub New()
+        MyBase.Multiline = True
+        MyBase.ScrollBars = ScrollBars.Both
+        MyBase.ReadOnly = True
+        MyBase.Font = New Font("Courier New", 8)
+    End Sub
+
+    Public Property TimeStampFormat() As String = "yyyy-MM-dd hh:nn:ss"
+
+    Public Sub Log(ByVal Text As String)
+        TextBuilder.AppendLine(Text)
+        MyBase.Text = TextBuilder.ToString
+    End Sub
+
+End Class
