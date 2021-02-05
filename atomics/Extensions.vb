5214df8f558a784d41cac821ec8f8185373ef9f1
Martin Weiss <albusmw@gmx.de>
2021-02-05 18:22:38 +0100

    overloads added

--- atomics/Extensions.vb
+++ atomics/Extensions.vb
@@ -254,6 +254,55 @@ Module DirectoryExtension
 
 End Module
 
+Module ConcurrentDictionaryExtensions
+
+    '''<summary>Sort the dictionary passed.</summary>
+    <Extension()>
+    Public Function SortDictionary(Of T1, T2)(ByRef Hist As Concurrent.ConcurrentDictionary(Of T1, T2)) As Concurrent.ConcurrentDictionary(Of T1, T2)
+        Return SortDictionary(Hist, False)
+    End Function
+
+    '''<summary>Sort the dictionary passed and invert the order.</summary>
+    <Extension()>
+    Public Function SortDictionaryInverse(Of T1, T2)(ByRef Hist As Concurrent.ConcurrentDictionary(Of T1, T2)) As Concurrent.ConcurrentDictionary(Of T1, T2)
+        Return SortDictionary(Hist, True)
+    End Function
+
+    '''<summary>Sort the dictionary passed.</summary>
+    <Extension()>
+    Public Function SortDictionary(Of T1, T2)(ByRef Hist As Concurrent.ConcurrentDictionary(Of T1, T2), ByVal Reverse As Boolean) As Concurrent.ConcurrentDictionary(Of T1, T2)
+        Dim DontCare1 As T1
+        Dim DontCare2 As T1
+        Return SortDictionary(Hist, Reverse, DontCare1, DontCare2)
+    End Function
+
+    '''<summary>Sort the dictionary passed.</summary>
+    <Extension()>
+    Public Function SortDictionary(Of T1, T2)(ByRef Hist As Concurrent.ConcurrentDictionary(Of T1, T2), ByVal Reverse As Boolean, ByRef Min As T1, ByRef Max As T1) As Concurrent.ConcurrentDictionary(Of T1, T2)
+
+        'Generate a list
+        Dim KeyList As New List(Of T1)
+        For Each Entry As T1 In Hist.Keys
+            KeyList.Add(Entry)
+        Next Entry
+        'Sort keys
+        KeyList.Sort()
+        If KeyList.Count > 0 Then
+            Min = KeyList(0)
+            Max = KeyList(KeyList.Count - 1)
+        End If
+        If Reverse Then KeyList.Reverse()
+        'Re-generate dictionary
+        Dim RetVal As New Concurrent.ConcurrentDictionary(Of T1, T2)
+        For Each Entry As T1 In KeyList
+            RetVal.TryAdd(Entry, Hist(Entry))
+        Next Entry
+        Return RetVal
+
+    End Function
+
+End Module
+
 Module ListExtensions
 
     '''<summary>Add the passed new element if it does not already exist.</summary>
